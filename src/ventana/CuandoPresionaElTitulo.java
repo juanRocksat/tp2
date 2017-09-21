8a677dd4dcfe101e2a45b1bf7ffd675f78b30af5
pacoDan1 <31734608+pacoDan1@users.noreply.github.com>
2017-09-21 18:15:04 -0300

    CuandoPresionaElTitulo.java 1
    CuandoPresionaElTitulo.java 2

---
+++ src/ventana/CuandoPresionaElTitulo.java
@@ -0,0 +1,19 @@
+package ventana;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+public class CuandoPresionaElTitulo implements ActionListener
+{
+
+	public void actionPerformed(ActionEvent e){
+		System.out.print("presionaste el boton titulo");
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
